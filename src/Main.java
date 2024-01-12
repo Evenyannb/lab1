f503ad692ea7ba3f1722c73cd106519d8067786a
yanlian <yanlian@iu.edu>
2024-01-12 11:38:27 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    int[] nums = {2,7,11,15};
+    int traget = 9;
+    int[] expected = {0,1};
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
